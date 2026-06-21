5f758b13139d4f0913e6c6a721a9e622920d5571
cesar2171 <cesarbus2171@gmail.com>
2026-06-21 17:03:23 -0600

    agregue mdelos en proveedores

--- proveedor/models.py
+++ proveedor/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class proveedor(models.Model):
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=200)
+    telefono = models.CharField(max_length=20)
+    email = models.EmailField()
+    productos_suministrados = models.TextField()
