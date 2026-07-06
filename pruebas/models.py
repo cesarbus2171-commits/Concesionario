2eb9f83c9528dca35a058c7999676eb35f9221fe
cesar2171 <cesarbus2171@gmail.com>
2026-07-06 15:34:55 -0600

    Pruebas

--- pruebas/models.py
+++ pruebas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class prueba(models.Model):
+    nombre_cliente = models.CharField(max_length=100)
+    fecha_prueba = models.DateField()
+    modelo_vehiculo = models.CharField(max_length=100)
+    empleado_encargado = models.CharField(max_length=100)
+    mecanico_asignado = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'pruebas_pruebas'
--- pruebas/views.py
+++ pruebas/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from .models import prueba
 
-def pruebas(request):
+def listapruebas(request):
+    pruebas = prueba.objects.all()
+    return render(request, 'pruebas/pruebas.html', {'pruebas': pruebas})
 
-    return render(request, 'pruebas/pruebas.html')
+def crearprueba(request):
+    if request.method == 'POST':
+        nombre_cliente = request.POST['nombre_cliente']
+        fecha_prueba = request.POST['fecha_prueba']
+        modelo_vehiculo = request.POST['modelo_vehiculo']
+        empleado_encargado = request.POST['empleado_encargado']
+        mecanico_asignado = request.POST['mecanico_asignado']
+
+        nueva_prueba = prueba(nombre_cliente=nombre_cliente, fecha_prueba=fecha_prueba, modelo_vehiculo=modelo_vehiculo, empleado_encargado=empleado_encargado, mecanico_asignado=mecanico_asignado)
+        nueva_prueba.save()
+        return redirect('/pruebas/')
